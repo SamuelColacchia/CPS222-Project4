3e93e98f209caddce066b793fa48492ed7d3df7d
mFelgate <m.felgate55@gmail.com>
2016-04-11 22:49:05 -0400

    insert with recusion: needs little work

--- BTree.cc
+++ BTree.cc
@@ -20,29 +20,72 @@ using std::cout;
 #include "BTreeBlock.h"
 
 BTree::BTree(string name)
-: _file(* new BTreeFile(name))
-{ }
+        : _file(*new BTreeFile(name))
+{
+}
 
 #ifndef PROFESSOR_VERSION
 
 void BTree::insert(string key, string value)
 {
-    // Student code goes here - remove this line
+        if (value == 0)
+                std::cout << "/* search fails */" << std::endl;
+
+        else{
+                int root = getRoot();
+                int currentBlock = getBlock(root);
+                while(isLeaf(currentBlock) == false) {
+                        currentBlock = getChild(getPosition(key));
+                }
+                BTreeBlock.insert(getPosition(key), key, value, BTreeFile.getChild(currentBlock))
+                if(splitNeeded(currentBlock) == false) {
+                    putBlock(currentBlock);
+                }
+                else{
+                        BTreeBlock.BTreeBlock();
+                        BTreeBlock.split(string & promotedKey, string & promotedValue,
+                               BTreeBlock & rightHalf);
+                        BTreeFile.putBlock();
+                        BTreeFile.allocateBlock();
+                        BTreeFile.putBlock();
+                        insertR((key, value, currentBlock);
+                }
+        }
 }
+void BTree::insertR(string key, string value, editedBlock){
+    int parent = getRoot();
+    int currentBlock = editedBlock;
+    while(currentBlock != getChild(root)){
+        int currentBlock = getChild(root);
+    }
+    BTreeBlock.insert(getPosition(key), key, value, BTreeFile.getChild(currentBlock))
+    if(splitNeeded(currentBlock) == false) {
+        putBlock(currentBlock);
+    }
+    else{
+        BTreeBlock();
+        split();
+        putBlock();
+        allocateBlock();
+        putBlock();
+        insertR(key, value, currentBlock);
+    }
+}
+
 
 bool BTree::lookup(string key, string & value) const
 {
-    return false; // Student code goes here - remove this line
+        return false; // Student code goes here - remove this line
 }
 
 bool BTree::remove(string key)
 {
-    return false; // Student code goes here - remove this line
+        return false; // Student code goes here - remove this line
 }
 
 #else
 
-#define QUOTE(Q) #Q
+#define QUOTE(Q) # Q
 #define INCLUDE_NAME(X) QUOTE(X)
 #include INCLUDE_NAME(PROFESSOR_VERSION)
 
@@ -50,23 +93,23 @@ bool BTree::remove(string key)
 
 void BTree::print() const
 {
-    cout << "BTree in file ";
-    _file.printHeaderInfo();
-    cout << endl;
+        cout << "BTree in file ";
+        _file.printHeaderInfo();
+        cout << endl;
 
-    BTreeFile::BlockNumber root = _file.getRoot();
-    if (root == 0)
-        cout << "Empty tree" << endl;
-    else
-        _file.printBlock(root, true, 1);
+        BTreeFile::BlockNumber root = _file.getRoot();
+        if (root == 0)
+                cout << "Empty tree" << endl;
+        else
+                _file.printBlock(root, true, 1);
 }
 
 void BTree::print(BTreeFile::BlockNumber blockNumber) const
 {
-    _file.printBlock(blockNumber, false, 1);
+        _file.printBlock(blockNumber, false, 1);
 }
 
 BTree::~BTree()
 {
-    delete (& _file);
+        delete (&_file);
 }
